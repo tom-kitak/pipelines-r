e312bcd3db9b57c61c6f072e1b566c5b7e0552bf
Atreya Choudhury <atreyachoudhury@hotmail.com>
2026-01-09 09:20:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Methods in R
+
+Methods, implemented in R, for the scRNA pipelines benchmark
---
+++ run.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+library(argparse)
+
+parser <- ArgumentParser(description = "Benchmarking entrypoint")
+
+parser$add_argument(
+  "--output_dir", "-o",
+  dest = "output_dir", type = "character",
+  help = "output directory where files will be saved",
+  default = getwd(), required = TRUE
+)
+parser$add_argument(
+  "--name", "-n",
+  dest = "name", type = "character",
+  help = "name of the module",
+  required = TRUE
+)
+parser$add_argument(
+  "--method_name",
+  dest = "method_name", type = "character",
+  help = "name of the method",
+  choices = c("osca", "scrapper", "seurat"), required = TRUE
+)
+
+args <- parser$parse_args()
