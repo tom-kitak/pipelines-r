c15af081ecc95d1f4c40d79039c181ecd6c9608a
RoseYuan <roseluosy@gmail.com>
2026-01-09 11:23:18 +0100

    update scrapper pipeline

--- scrapper_sc_mix.R
+++ scrapper_sc_mix.R
@@ -19,148 +19,127 @@ rownames(time) <- c("find_mit_gene", "filter", "normalization", "hvg",
 
 #### data ####
 # load("/mnt/spca/pipeline_sc/sc_mix.RData")
-load("sc_mix.RData")
-sce <- sce_sc_10x_5cl_qc
-table(sce$cell_line)
-assay(sce) <- DelayedArray(assay(sce))
+run_scrapper <- function(sce) {
+    assay(sce) <- DelayedArray(assay(sce))
 
-#### 1. find mithocondial genes  ####
+    #### 1. find mithocondial genes  ####
 
-start_time <- Sys.time()
+    start_time <- Sys.time()
 
-is.mito <- grepl("^MT-", rownames(sce))
-table(is.mito)
+    is.mito <- grepl("^MT-", rownames(sce))
 
-rna.qc.metrics <- computeRnaQcMetrics(assay(sce), subsets=list(mt=is.mito), 
-                                      num.threads=nthreads)
+    rna.qc.metrics <- computeRnaQcMetrics(assay(sce), subsets=list(mt=is.mito), 
+                                        num.threads=nthreads)
 
-rna.qc.thresholds <- suggestRnaQcThresholds(rna.qc.metrics)
-rna.qc.filter <- filterRnaQcMetrics(rna.qc.thresholds, rna.qc.metrics)
+    rna.qc.thresholds <- suggestRnaQcThresholds(rna.qc.metrics)
+    rna.qc.filter <- filterRnaQcMetrics(rna.qc.thresholds, rna.qc.metrics)
 
-filtered <- sce[,rna.qc.filter,drop=FALSE]
+    filtered <- sce[,rna.qc.filter,drop=FALSE]
 
 
-end_time <- Sys.time()
-time_elapsed <- end_time - start_time
-print(paste("Time Elapsed:", time_elapsed))
-time[1,1] <- time_elapsed
+    end_time <- Sys.time()
+    time_elapsed <- end_time - start_time
+    print(paste("Find mithocondrial genes. Time Elapsed:", time_elapsed))
+    time[1,1] <- time_elapsed
 
-# filter data ####
-start_time <- Sys.time()
-sce 
-end_time <- Sys.time()
-time_elapsed <- end_time - start_time
-print(paste("Time Elapsed:", time_elapsed))
-time[2,1] <- time_elapsed
+    # filter data ####
+    time[2,1] <- NA
 
-# normalization ####
-start_time <- Sys.time()
+    # normalization ####
+    start_time <- Sys.time()
 
-size.factors <- centerSizeFactors(rna.qc.metrics$sum[rna.qc.filter])
-normalized <- normalizeCounts(assay(filtered), size.factors)
+    size.factors <- centerSizeFactors(rna.qc.metrics$sum[rna.qc.filter])
+    normalized <- normalizeCounts(assay(filtered), size.factors)
 
-end_time <- Sys.time()
-time_elapsed <- end_time - start_time
-print(paste("Time Elapsed:", time_elapsed))
-time[3,1] <- time_elapsed
+    end_time <- Sys.time()
+    time_elapsed <- end_time - start_time
+    print(paste("Normalization. Time Elapsed:", time_elapsed))
+    time[3,1] <- time_elapsed
 
-assay(filtered, "normalized") <- normalized
+    assay(filtered, "normalized") <- normalized
 
-# Identification of highly variable features (feature selection) ####
-start_time <- Sys.time()
-gene.var <- modelGeneVariances(assay(filtered, "normalized"), num.threads=nthreads)
-hvg.sce.var <- chooseHighlyVariableGenes(gene.var$statistics$residuals, top = 1000)
+    # Identification of highly variable features (feature selection) ####
+    start_time <- Sys.time()
+    gene.var <- modelGeneVariances(assay(filtered, "normalized"), num.threads=nthreads)
+    hvg.sce.var <- chooseHighlyVariableGenes(gene.var$statistics$residuals, top = 1000)
 
-# hvg.sce.var <- rownames(filtered[hvg.sce.var,])
+    # hvg.sce.var <- rownames(filtered[hvg.sce.var,])
 
 
-end_time <- Sys.time()
-time_elapsed <- end_time - start_time
-print(paste("Time Elapsed:", time_elapsed))
-time[4,1] <- time_elapsed
+    end_time <- Sys.time()
+    time_elapsed <- end_time - start_time
+    print(paste("Identification of highly variable features. Time Elapsed:", time_elapsed))
+    time[4,1] <- time_elapsed
 
 
-#save(list = "hvg.sce.var", file = "sc_mix_scrapper_hvg.RData")
+    #save(list = "hvg.sce.var", file = "sc_mix_scrapper_hvg.RData")
 
-# PCA ####
-start_time <- Sys.time()
+    # PCA ####
+    start_time <- Sys.time()
 
-pca <- runPca((assay(filtered, "normalized")[hvg.sce.var,]), num.threads=nthreads, number = 50)
+    pca <- runPca((assay(filtered, "normalized")[hvg.sce.var,]), num.threads=nthreads, number = 50)
 
-end_time <- Sys.time()
-time_elapsed <- end_time - start_time
-print(paste("Time Elapsed:", time_elapsed))
-time[6,1] <- time_elapsed
+    end_time <- Sys.time()
+    time_elapsed <- end_time - start_time
+    print(paste("PCA. Time Elapsed:", time_elapsed))
+    time[6,1] <- time_elapsed
 
-reducedDim(filtered, "PCA") <- t(pca$components)
-dim(reducedDim(filtered, "PCA")[,1:2])
-pca_var_pct_scrap <- pca$variance.explained
-# t-sne ####
-start_time <- Sys.time()
+    reducedDim(filtered, "PCA") <- t(pca$components)
+    dim(reducedDim(filtered, "PCA")[,1:2])
+    pca_var_pct_scrap <- pca$variance.explained
+    # t-sne ####
+    start_time <- Sys.time()
 
-tsne.out <- runTsne(pca$components, num.threads=nthreads)
+    tsne.out <- runTsne(pca$components, num.threads=nthreads)
 
-end_time <- Sys.time()
-time_elapsed <- end_time - start_time
-print(paste("Time Elapsed:", time_elapsed))
-time[7,1] <- time_elapsed
+    end_time <- Sys.time()
+    time_elapsed <- end_time - start_time
+    print(paste("t-sne. Time Elapsed:", time_elapsed))
+    time[7,1] <- time_elapsed
 
-reducedDim(filtered, "TSNE") <- tsne.out
+    reducedDim(filtered, "TSNE") <- tsne.out
 
-plotTSNE(filtered, color_by = "cell_line")
+    # umap ####
+    start_time <- Sys.time()
+    set.seed(1000000)
 
+    umap.out <- runUmap(pca$components, num.threads=nthreads)
 
-# umap ####
-start_time <- Sys.time()
-set.seed(1000000)
+    end_time <- Sys.time()
+    time_elapsed <- end_time - start_time
+    print(paste("UMAP. Time Elapsed:", time_elapsed))
+    time[8,1] <- time_elapsed
 
-umap.out <- runUmap(pca$components, num.threads=nthreads)
+    reducedDim(filtered, "UMAP") <- umap.out
 
-end_time <- Sys.time()
-time_elapsed <- end_time - start_time
-print(paste("Time Elapsed:", time_elapsed))
-time[8,1] <- time_elapsed
-
-reducedDim(filtered, "UMAP") <- tsne.out
-
-plotUMAP(filtered, color_by = "cell_line")
+    plotUMAP(filtered, color_by = "cell_line")
 
 
 
-# louvain  ####
-start_time <- Sys.time()
+    # louvain  ####
+    start_time <- Sys.time()
 
-snn.graph <- buildSnnGraph(pca$components, num.threads=nthreads)
-clust.out <- clusterGraph(snn.graph, method = c("multilevel"), multilevel.resolution = 0.18)
-end_time <- Sys.time()
-time_elapsed <- end_time - start_time
-print(paste("Time Elapsed:", time_elapsed))
-time[9,1] <- time_elapsed
+    snn.graph <- buildSnnGraph(pca$components, num.threads=nthreads)
+    clust.out <- clusterGraph(snn.graph, method = c("multilevel"), multilevel.resolution = 0.18)
+    end_time <- Sys.time()
+    time_elapsed <- end_time - start_time
+    print(paste("Louvain clusterings. Time Elapsed:", time_elapsed))
+    time[9,1] <- time_elapsed
 
+    louvain_clustering <- clust.out$membership
 
-
-length(clust.out$membership)
-length(filtered$cell_line)
-ARI <- adjustedRandIndex((filtered$cell_line), clust.out$membership)
-ARI
-cat("Leiden Adjusted Rand Index:", ARI, "\n")
-
-# leiden ####
-start_time <- Sys.time()
-snn.graph <- buildSnnGraph(pca$components, num.threads=nthreads)
-clust.out <- clusterGraph(snn.graph, method = c("leiden"), leiden.resolution = 0.16)
-end_time <- Sys.time()
-time_elapsed <- end_time - start_time
-print(paste("Time Elapsed:", time_elapsed))
-time[10,1] <- time_elapsed
-
-
-length(clust.out$membership)
-length(filtered$cell_line)
-ARI <- adjustedRandIndex((filtered$cell_line), clust.out$membership)
-ARI
-cat("Leiden Adjusted Rand Index:", ARI, "\n")
-
-time 
-
-# save.image("sc_scrapper.RData")
+    # leiden ####
+    start_time <- Sys.time()
+    snn.graph <- buildSnnGraph(pca$components, num.threads=nthreads)
+    clust.out <- clusterGraph(snn.graph, method = c("leiden"), leiden.resolution = 0.16)
+    leiden_clustering <- clust.out$membership
+    end_time <- Sys.time()
+    time_elapsed <- end_time - start_time
+    print(paste("Leiden clusterings. Time Elapsed:", time_elapsed))
+    time[10,1] <- time_elapsed
+    return(list(pca = reducedDim(filtered, "PCA"), 
+                time = time, 
+                leiden= leiden_clustering, 
+                louvin= louvain_clustering))
+    # save.image("sc_scrapper.RData")
+}
